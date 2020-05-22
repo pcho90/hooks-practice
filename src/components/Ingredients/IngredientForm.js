0bc742030cb28557993434d014960ef8a4f8fc88
Peter Cho <pcho51990@gmail.com>
2020-05-21 23:30:59 -0400

    added search function and fetching data from firebase

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title, amount });
   };
 
   return (
-    <section className="ingredient-form">
+    <section className='ingredient-form'>
       <Card>
         <form onSubmit={submitHandler}>
-          <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+          <div className='form-control'>
+            <label htmlFor='title'>Name</label>
+            <input
+              type='text'
+              id='title'
+              value={title}
+              onChange={event => setTitle(event.target.value)}
+            />
           </div>
-          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+          <div className='form-control'>
+            <label htmlFor='amount'>Amount</label>
+            <input
+              type='number'
+              id='amount'
+              value={amount}
+              onChange={event => setAmount(event.target.value)}
+            />
           </div>
-          <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
+          <div className='ingredient-form__actions'>
+            <button type='submit'>Add Ingredient</button>
           </div>
         </form>
       </Card>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]);
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://hooks-practice-730ba.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = id => {
+    setUserIngredients(prevIngredients =>
+      prevIngredients.filter(item => item.id !== id)
+    );
+  };
+
   return (
-    <div className="App">
-      <IngredientForm />
+    <div className='App'>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
